01ac5af578ef5539f8662bdb4d69a0b3c2d66ab9
Natuel777 <113438331+Natuel777@users.noreply.github.com>
2022-09-15 12:09:41 -0300

    Add files via upload

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float offset = 2f;
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Bola");
+        transform.Rotate(70,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y +6, player.transform.position.z - offset);
+        
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed = 6f;
+    Vector3 posInicial;
+    Vector3 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        posInicial = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        rb.AddForce(input*moveSpeed);
+
+        if(transform.position.y<-2){Respawn();}
+    }
+
+    void Respawn(){
+        transform.position = posInicial;
+    }
+}
